84392f6bb0bed6c0f00227b85e733a19cc2d7cf9
Jesus Angel Sandoval <114720772+iesus-s@users.noreply.github.com>
2023-11-28 20:15:56 -0800

    input files

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ import sys
 if __name__ == '__main__':
     # Reading Text File
     text_file = sys.argv[1]
-    text_input = pd.read_csv(text_file, sep=" ", header=None)
+    text_input = pd.read_csv(text_file, sep="\t", header=None)
+
+    # Unpack First Process
+    executing = text_input.iloc[1]
+    virtual_page_number = bin(executing[1])[:7]
+    offset = bin(executing[1])[9:]
+    print(executing)
+    print(bin(executing[1]))
+    print("Virtual Page Number: ", virtual_page_number)
+    print("Offset: ", offset)
